a1f0a6c7d4d308104a3ee9418c59d7a1bcfd52bd
Dk-variya007 <variyadivyesh79@gmail.com>
2024-07-30 21:25:38 +0530

    update code

--- lib/view/billing_page.dart
+++ lib/view/billing_page.dart
@@ -1,9 +1,165 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_setting_page/helper/custome_divider.dart';
 
-class BillingTab extends StatelessWidget {
+class BillingTab extends StatefulWidget {
   const BillingTab({super.key});
 
+  @override
+  State<BillingTab> createState() => _BillingTabState();
+}
+
+class _BillingTabState extends State<BillingTab> {
+  final TableRow _tableRow1 = const TableRow(children: [
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Features',
+          style: TextStyle(color: Colors.white),
+        )),
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Free',
+          style: TextStyle(color: Colors.white),
+        )),
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Pro',
+          style: TextStyle(color: Colors.white),
+        )),
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Enterprise',
+          style: TextStyle(color: Colors.white),
+        )),
+  ]);
+  final TableRow _tableRow2 = const TableRow(children: [
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Credits',
+          style: TextStyle(color: Colors.white),
+        )),
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Limited(200 monthly)',
+          style: TextStyle(color: Colors.white),
+        )),
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Unlimited',
+          style: TextStyle(color: Colors.white),
+        )),
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Unlimited',
+          style: TextStyle(color: Colors.white),
+        )),
+  ]);
+  final TableRow _tableRow3 = const TableRow(children: [
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Resolution',
+          style: TextStyle(color: Colors.white),
+        )),
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          '360p,720p',
+          style: TextStyle(color: Colors.white),
+        )),
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          '1080p',
+          style: TextStyle(color: Colors.white),
+        )),
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          '1080p',
+          style: TextStyle(color: Colors.white),
+        )),
+  ]);
+  final TableRow _tableRow4 = const TableRow(children: [
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Images',
+          style: TextStyle(color: Colors.white),
+        )),
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Al Generated',
+          style: TextStyle(color: Colors.white),
+        )),
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Al Generated, Manual',
+          style: TextStyle(color: Colors.white),
+        )),
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Al Generated, Manual',
+          style: TextStyle(color: Colors.white),
+        )),
+  ]);
+  final TableRow _tableRow5 = const TableRow(children: [
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Export Formats',
+          style: TextStyle(color: Colors.white),
+        )),
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'PNG',
+          style: TextStyle(color: Colors.white),
+        )),
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'PNG, WEBP',
+          style: TextStyle(color: Colors.white),
+        )),
+    Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'PNG, JPEG, WEBP',
+          style: TextStyle(color: Colors.white),
+        )),
+  ]);
+  final TableRow _tableRow6 = TableRow(children: [
+    const Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          '',
+        )),
+    const Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(
+          '',
+        )),
+    Padding(
+      padding: const EdgeInsets.all(10),
+      child:
+          ElevatedButton(onPressed: () {}, child: const Text('Upgrade to PRO')),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(10),
+      child: ElevatedButton(
+          onPressed: () {}, child: const Text('Upgrade to ENTERPRISE')),
+    )
+  ]);
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -15,130 +171,40 @@ class BillingTab extends StatelessWidget {
               'You are using the FREE VERSION of Product',
               style: TextStyle(color: Colors.white),
             ),
-            trailing: ElevatedButton(
-                onPressed: () {}, child: const Text('Upgrade plan')),
+            trailing: SizedBox(
+              width: 150,
+              child: ElevatedButton(
+                  onPressed: () {}, child: const Text('Upgrade plan')),
+            ),
           ),
           const ListTile(
             title: Text('Monthly Credits left'),
             trailing: Text('150 of 200'),
           ),
-          const Divider(),
-          DataTable(
-            columns: const [
-              DataColumn(
-                  label: Text(
-                'Features',
-                style: TextStyle(color: Colors.white),
-              )),
-              DataColumn(
-                  label: Text(
-                'Free',
-                style: TextStyle(color: Colors.white),
-              )),
-              DataColumn(
-                  label: Text(
-                'Pro',
-                style: TextStyle(color: Colors.white),
-              )),
-              DataColumn(
-                  label: Text(
-                'Enterprise',
-                style: TextStyle(color: Colors.white),
-              )),
-            ],
-            rows: [
-              const DataRow(cells: [
-                DataCell(Text(
-                  'Credits',
-                  style: TextStyle(color: Colors.white),
-                )),
-                DataCell(Text(
-                  'Limited (200 monthly)',
-                  style: TextStyle(color: Colors.white),
-                )),
-                DataCell(Text(
-                  'Unlimited',
-                  style: TextStyle(color: Colors.white),
-                )),
-                DataCell(Text(
-                  'Unlimited',
-                  style: TextStyle(color: Colors.white),
-                )),
-              ]),
-              const DataRow(cells: [
-                DataCell(Text(
-                  'Resolution',
-                  style: TextStyle(color: Colors.white),
-                )),
-                DataCell(Text(
-                  '360p, 720p',
-                  style: TextStyle(color: Colors.white),
-                )),
-                DataCell(Text(
-                  '1080p',
-                  style: TextStyle(color: Colors.white),
-                )),
-                DataCell(Text(
-                  '1080p',
-                  style: TextStyle(color: Colors.white),
-                )),
-              ]),
-              const DataRow(cells: [
-                DataCell(Text(
-                  'Images',
-                  style: TextStyle(color: Colors.white),
-                )),
-                DataCell(Text(
-                  'AI Generated',
-                  style: TextStyle(color: Colors.white),
-                )),
-                DataCell(Text(
-                  'AI Generated, Manual',
-                  style: TextStyle(color: Colors.white),
-                )),
-                DataCell(Text(
-                  'AI Generated, Manual',
-                  style: TextStyle(color: Colors.white),
-                )),
-              ]),
-              const DataRow(cells: [
-                DataCell(Text(
-                  'Export Formats',
-                  style: TextStyle(color: Colors.white),
-                )),
-                DataCell(Text(
-                  'PNG',
-                  style: TextStyle(color: Colors.white),
-                )),
-                DataCell(Text(
-                  'PNG, WEBP',
-                  style: TextStyle(color: Colors.white),
-                )),
-                DataCell(Text(
-                  'PNG, JPEG, WEBP',
-                  style: TextStyle(color: Colors.white),
-                )),
-              ]),
-              DataRow(cells: [
-                const DataCell(Text(
-                  '',
-                )),
-                const DataCell(Text(
-                  '',
-                )),
-                DataCell(
-                  ElevatedButton(
-                      onPressed: () {}, child: const Text('Upgrade to PRO')),
-                ),
-                DataCell(
-                  ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Upgrade to ENTERPRISE')),
-                )
-              ]),
+          const Text(
+            "Plans",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(
+            color: Colors.grey,
+            height: 1,
+          ),
+          Table(
+            children: [_tableRow1],
+          ),
+          Table(
+            border: TableBorder.all(color: Colors.grey),
+            children: [
+              _tableRow2,
+              _tableRow3,
+              _tableRow4,
+              _tableRow5,
+              _tableRow6
             ],
           ),
-          const Divider()
         ],
       ),
     );
